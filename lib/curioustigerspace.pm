950920baa66a192d7c4440ec005f7639b1c10f48
MrFabkirox <fabrice.esope@gmail.com>
2016-08-15 18:05:00 +0100

    notion page

--- lib/curioustigerspace.pm
+++ lib/curioustigerspace.pm
@@ -7,4 +7,30 @@ get '/' => sub {
     template 'index';
 };
 
+get '/page1' => sub {
+    template 'layouts/page1/page1';
+};
+
+get qr/^[0-9]*$}/  => sub {
+    template 'layouts/page1/page1';
+};
+
+get qr{s/page1/} => sub {
+    template 'layouts/page1/page1'
+};
+
+get qr{/\d/([\w]+)} => sub {
+    template 'layouts/page1/page1'
+};
+
+get qr{/hello/([\w]+)} => sub {
+    my ($name) = splat;
+    return "Hello $name";
+};
+
+get qr{/hello/([\w]+)} => sub {
+    my ($name) = splat;
+    return "Hello $name";
+};
+
 true;
