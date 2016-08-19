bf244fcb75287e9942c5fc22ea3a84de668a0cb2
MrFabkirox <fabrice.esope@gmail.com>
2016-08-19 11:13:53 +0100

    page1 modif

--- lib/curioustigerspace.pm
+++ lib/curioustigerspace.pm
@@ -11,16 +11,8 @@ get '/page1' => sub {
     template 'layouts/page1/page1';
 };
 
-get qr/^[0-9]*$}/  => sub {
-    template 'layouts/page1/page1';
-};
-
-get qr{s/page1/} => sub {
-    template 'layouts/page1/page1'
-};
-
-get qr{/\d/([\w]+)} => sub {
-    template 'layouts/page1/page1'
+get qr/pages?[2-9]/ => sub {
+    template 'layouts/page2/page2';
 };
 
 get qr{/hello/([\w]+)} => sub {
